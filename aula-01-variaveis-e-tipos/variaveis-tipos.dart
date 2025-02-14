f7ed00b861a2170d739d2fe4cac05bec37be2934
Luiz Fernando <fernandinho.manon@gmail.com>
2025-02-14 20:39:35 -0300

    feat: Adicionar arquivo da aula 01 sobre variáveis e tipos

---
+++ aula-01-variaveis-e-tipos/variaveis-tipos.dart
@@ -0,0 +1,26 @@
+void main() {
+
+  // Cria uma variável chamada "nome" do tipo String (texto) e guarda o valor "Luiz Fernando" dentro dela
+  String nome = 'Luiz Fernando';
+  
+  // Imprime o valor da variável "nome" na tela
+  print(nome);
+
+  // Criar uma variável chamada "idade" do tipo int (número) e guarda o valor 22 dentro dela
+  int idade = 22;
+ 
+  // Imprime o valor da variável "idade" na tela.
+  print(idade);
+
+  // Criar uma variável chamada "programador" do tipo bool (verdadeiro ou falso) e guarda o valor "true" (verdadeiro) dentro dela
+  bool programador = true;
+  
+  // Imprime o valor da variável "programador" na tela.
+  print(programador);
+
+  //  Cria uma lista chamada "palavras" que contém duas Strings: "Luiz" e "Fernando"
+  List<String> palavras = ['Luiz', 'Fernando'];
+  
+  // Imprime o primeiro elemento da lista (índice 0) e o segundo elemento da lista (índice 1) na tela, separador por um hífen.
+  print('${palavras[0]} - ${palavras[1]}');
+}
